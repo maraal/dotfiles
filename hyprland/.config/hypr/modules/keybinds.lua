aa9f71d4f9c013e4032ec22edc66164c3b2334ac
Lucas Oliveira <lucasmaraal@gmail.com>
2026-05-18 19:45:31 -0300

    feat: hyprshutdown instalado

--- hyprland/.config/hypr/modules/keybinds.lua
+++ hyprland/.config/hypr/modules/keybinds.lua
@@ -5,6 +5,8 @@ local mainMod = "SUPER"
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
 
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+
 for i = 1, 10 do
     local key = i % 10
     hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
@@ -20,6 +22,7 @@ hl.bind(mainMod .. " + k", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + V", hl.dsp.exec_cmd("cliphist list | fuzzel --dmenu | cliphist decode | wl-copy"))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd("foot bluetuith"))
+hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd("fuzzel"))
 
 -- Screenshots
 hl.bind("Print", hl.dsp.exec_cmd("hyprcap shot region -c -w"))
